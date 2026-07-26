86bbc4daeb18b0947ea1c44f33d5c5e76da88114
stelum <frostdgs2000@gmail.com>
2026-07-27 00:10:49 +0300

    Add files via upload

--- config/animations.lua
+++ config/animations.lua
@@ -1,7 +1,7 @@
--- Кривые
-hl.curve("fast", { type = "bezier", points = { {0.15, 0.85}, {0.3, 1} } })
+-- Идеально сбалансированная кривая: мягкий старт и плавное закругление в конце
+hl.curve("smooth_slide", { type = "bezier", points = { {0.1, 0.9}, {0.2, 1.0} } })
 
--- Анимации
-hl.animation({ leaf = "global",     enabled = true, speed = 6, bezier = "fast" })
-hl.animation({ leaf = "windows",    enabled = true, speed = 5, bezier = "fast", style = "popin 80%" })
-hl.animation({ leaf = "workspaces", enabled = true, speed = 6, bezier = "fast", style = "fade" })
+-- Анимации (везде чистый slide, но с крутой кривой)
+hl.animation({ leaf = "global",     enabled = true, speed = 4, bezier = "smooth_slide" })
+hl.animation({ leaf = "windows",    enabled = true, speed = 4, bezier = "smooth_slide", style = "slide" })
+hl.animation({ leaf = "workspaces", enabled = true, speed = 3, bezier = "smooth_slide", style = "slide" })
