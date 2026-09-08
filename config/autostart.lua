6e6e2a6cbc023e413c8076a16e024db8100ce0c8
stelum <frostdgs2000@gmail.com>
2026-09-08 17:21:58 +0300

    Delete config directory

--- config/autostart.lua
+++
@@ -1,9 +0,0 @@
--- Auto-start config
--- if you dont use UWSM add your auto start programs here, otherwise use XDG autostart https://wiki.archlinux.org/title/XDG_Autostart
-
-hl.on("hyprland.start", function ()
-    hl.exec_cmd("dbus-update-activation-environment --systemd --all")
-    hl.exec_cmd("systemctl --user start hyprland-session.target graphical-session.target")
-    hl.exec_cmd("noctalia") -- Чистый запуск Noctalia v5 без quicshell/qs
-    hl.exec_cmd("xhost +SI:localuser:root")
-end)
--- config/defaults.lua
+++
@@ -1,7 +0,0 @@
--- Hyprland default apps
-
-TERMINAL     = "ghostty"
-FILE_MANAGER = "nautilus"
-BROWSER      = "zen-beta"
-EDITOR       = "gnome-text-editor --new-window"
-CALCULATOR   = "gnome-calculator"
